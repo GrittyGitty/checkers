51a62751378f049c3e44dd9745c1130cdc7808b4
Netanel Haber <netanel.t.haber@gmail.com>
2022-10-15 21:42:52 +0300

    added github link

---
+++ insert_sha.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env zx
+const replace = require("replace-in-file");
+const sha = String(await $`git rev-parse --short HEAD`).trim();
+
+await replace({
+  files: "index.html",
+  from: /data-head-during-build=".*"/,
+  to: `data-head-during-build="${sha}"`
+});
