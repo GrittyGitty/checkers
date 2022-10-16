760d94926b39e48433abe9b460a63b03f8a6f35c
Netanel Haber <netanel.t.haber@gmail.com>
2022-10-16 14:18:34 +0300

    reliable caching

--- build_scripts/copy_files_into_dist.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zx
-
-$.verbose = false
-
-await Promise.all([
-  $`cp index.css dist/index.css`,
-  $`cp index.js dist/index.js`,
-  $`cp commit.js dist/commit.js`,
-]);
---
+++ pkg.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env zx
+
+$.verbose = false
+
+
+const replace = require("replace-in-file");
+const sha = String(await $`git rev-parse --short HEAD`).trim();
+
+const move = (file) => $`mv ${file} dist/${sha}.${file}`;
+const copy = (file) => $`cp ${file} dist/${sha}.${file}`;
+
+await Promise.all([
+  replace({
+    files: "index.html",
+    from: [/data-head-during-build=".*"/, new RegExp(`(?<=dist\/)[^.]*?\\.(?=.*(js|css))`, "g")],
+    to: [`data-head-during-build="${sha}"`, `${sha}.`]
+  }),
+  copy("index.css"),
+  copy("index.js"),
+  copy("commit.js"),
+  move("index.min.js").then(() => replace({
+    files: `dist/${sha}.index.min.js`,
+    from: new RegExp("(?<=sourceMappingURL=).*?\\.(?=index)"),
+    to: `${sha}.`
+  })),
+  move("index.min.js.map").then(() => replace({
+    files: `dist/${sha}.index.min.js.map`,
+    from: new RegExp("index.js"),
+    to: `${sha}.index.js`
+  }))]
+);
