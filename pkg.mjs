05b39df7fec59ab5ee60aed1a7a34251e1554c22
Netanel Haber <netanel.t.haber@gmail.com>
2022-10-16 21:30:51 +0300

    uglifyjs is in zx too

--- pkg.mjs
+++ pkg.mjs
@@ -4,29 +4,24 @@ $.verbose = false
 
 import replace from "replace-in-file";
 
+const deleteOldFiles = $`find dist -maxdepth 1 -type f -delete`;
 const sha = String(await $`git rev-parse --short HEAD`).trim();
 
-const move = (file) => $`mv ${file} dist/${sha}.${file}`;
 const copy = (file) => $`cp ${file} dist/${sha}.${file}`;
 
+
+const uglifyJs = $`uglifyjs index.js -cmo dist/${sha}.index.min.js --source-map url=index.min.js.map`;
+const replaceHtmlWithNewSha = replace({
+  files: "index.html",
+  from: [/data-head-during-build=".*"/, new RegExp(`(?<=dist\/)[^.]*?\\.(?=.*(js|css))`, "g")],
+  to: [`data-head-during-build="${sha}"`, `${sha}.`]
+});
+
 await Promise.all([
-  $`find dist -maxdepth 1 -type f -delete`,
-  replace({
-    files: "index.html",
-    from: [/data-head-during-build=".*"/, new RegExp(`(?<=dist\/)[^.]*?\\.(?=.*(js|css))`, "g")],
-    to: [`data-head-during-build="${sha}"`, `${sha}.`]
-  }),
+  deleteOldFiles,
+  uglifyJs,
+  replaceHtmlWithNewSha,
   copy("index.css"),
   copy("index.js"),
-  copy("commit.js"),
-  move("index.min.js").then(() => replace({
-    files: `dist/${sha}.index.min.js`,
-    from: new RegExp("(?<=sourceMappingURL=).*?\\.(?=index)"),
-    to: `${sha}.`
-  })),
-  move("index.min.js.map").then(() => replace({
-    files: `dist/${sha}.index.min.js.map`,
-    from: new RegExp("index.js"),
-    to: `${sha}.index.js`
-  }))]
-);
+  copy("commit.js")
+]);
