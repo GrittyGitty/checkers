e5ec63bffee28cbc2b00613d55ba27db8624e485
Netanel Haber <netanel.t.haber@gmail.com>
2022-10-17 11:43:37 +0300

    only create new dist files if changed

--- pkg.mjs
+++ pkg.mjs
@@ -2,26 +2,29 @@
 
 $.verbose = false
 
-import replace from "replace-in-file";
+import { hasChanges, transform, replaceHTML, embedShaIntoScript } from './scripts/utils.mjs';
+import { COMMIT, CSS, JS, MAP, MIN } from './scripts/consts.mjs';
 
-const deleteOldFiles = $`find dist -maxdepth 1 -type f -delete`;
-const sha = String(await $`git rev-parse --short HEAD`).trim();
+
+const [sha, beforeHead] = await Promise.all([$`git rev-parse --short HEAD`, $`git rev-parse --short HEAD~1`].map((s) => transform(s)));
+
+
 
 const copy = (file) => $`cp ${file} dist/${sha}.${file}`;
-
-
-const uglifyJs = $`uglifyjs index.js -cmo dist/${sha}.index.min.js --source-map url=index.min.js.map`;
-const replaceHtmlWithNewSha = replace({
-  files: "index.html",
-  from: [/data-head-during-build=".*"/, new RegExp(`(?<=dist\/)[^.]*?\\.(?=.*(js|css))`, "g")],
-  to: [`data-head-during-build="${sha}"`, `${sha}.`]
-});
+const rm = (files) => Promise.all(files.map(f => $`rm -f dist/${beforeHead}.${f}`));
+const seq = (main, { embed = main, rem = [main] } = {}) => [copy(main), embedShaIntoScript(sha, embed), rm(rem)];
 
 await Promise.all([
-  deleteOldFiles,
-  uglifyJs,
-  replaceHtmlWithNewSha,
-  copy("index.css"),
-  copy("index.js"),
-  copy("commit.js")
+  replaceHTML(
+    /data-head-during-build=".*"/,
+    `data-head-during-build="${sha}"`
+  ),
+  hasChanges(JS).then((has) => has && Promise.all([
+    $`uglifyjs ${JS} -cmo dist/${sha}.${MIN} --source-map url=${MAP}`,
+    ...seq(JS, { embed: MIN, rem: [JS, MIN, MAP] })
+  ])),
+  hasChanges(CSS).then((has) => has && Promise.all(seq(CSS))),
+  hasChanges(COMMIT).then(has => has && Promise.all(seq(COMMIT))),
 ]);
+
+
